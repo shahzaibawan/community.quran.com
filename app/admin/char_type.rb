335f182d9ef3e0ef8cc5afb4cebdf4b213125de1
shahzaib <shahzaibawan16@gmail.com>
2017-01-17 03:24:11 +0500

    theme setup

--- app/admin/char_type.rb
+++
@@ -1,7 +0,0 @@
-ActiveAdmin.register CharType do
-  menu parent: "Settings", priority: 10
-
-  permit_params do
-    [:name, :parent_id, :description]
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: "questions#index"
+  
+  resources :tags, only: [:index]
+  resources :questions, only: [:index, :show]
+  resources :users, only: [:index, :show]
+  resources :categories, only: [:index]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
